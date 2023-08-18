8be7ffa7ca2f801386c30ec90c998f8cb687403f
Username-Bogdan <127837806+Bogdan-Go-It@users.noreply.github.com>
2023-08-18 06:16:27 +0300

    fin

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// console.log(galleryItems);
+const takeUlGallery = document.querySelector('.gallery');
+const galeryImeg = createGalleryImag(galleryItems);
+function createGalleryImag(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img
+         class="gallery__image"
+         src="${preview}"
+         alt="${description}"
+         />
+      </a>
+    </li>`;
+    })
+    .join('');
+}
 
+takeUlGallery.insertAdjacentHTML('beforeend', galeryImeg);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: 'true',
+  captionsData: 'alt',
+  //   captionPosition: 'bottom',
+  captionDelay: 250,
+});
 console.log(galleryItems);
